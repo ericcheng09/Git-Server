39b188c21781746926255e5507d4965c0e36e0ea
鄭富元 <ericcheng@iii.org.tw>
2022-03-03 11:49:08 +0800

    skip initialization if found existed htpasswd file.

--- create-git-repo.sh
+++ create-git-repo.sh
@@ -1,12 +1,17 @@
 GIT_DIR="/srv/git/repos"
-htpasswd -bc /srv/git/.htpasswd $REPO_USER $REPO_PASS
-REPO_NAME=$REPO
-PREFIX=$PREFIX
-sed -i "1i Define PREFIX ${PREFIX}" /etc/apache2/sites-available/git-site.conf
-mkdir -p "${GIT_DIR}/${REPO_NAME}.git"
-cd "${GIT_DIR}/${REPO_NAME}.git"
-git init --bare
-git config http.receivepack true
-git update-server-info
-chown -Rf www-data:www-data "${GIT_DIR}/${REPO_NAME}.git"
-echo "Git repository '${REPO_NAME}' created in ${GIT_DIR}/${REPO_NAME}.git"
+if [ -f /srv/git/.htpasswd ]
+then
+  echo "Found existed git repo, skip initalization"
+else
+  htpasswd -bc /srv/git/.htpasswd $REPO_USER $REPO_PASS
+  REPO_NAME=$REPO
+  PREFIX=$PREFIX
+  sed -i "1i Define PREFIX ${PREFIX}" /etc/apache2/sites-available/git-site.conf
+  mkdir -p "${GIT_DIR}/${REPO_NAME}.git"
+  cd "${GIT_DIR}/${REPO_NAME}.git"
+  git init --bare
+  git config http.receivepack true
+  git update-server-info
+  chown -Rf www-data:www-data "${GIT_DIR}/${REPO_NAME}.git"
+  echo "Git repository '${REPO_NAME}' created in ${GIT_DIR}/${REPO_NAME}.git"
+fi
